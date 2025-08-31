ab9409ea3da6f605c2a14ea2e9f623e2cf5a7c5a
Artem Strelnikov <piramida1237@gmail.com>
2025-08-31 21:08:06 +0200

    function

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -2,4 +2,12 @@ function sum(a, b) {
   return a + b;
 }
 
+function seyHello() {
+  console.log('hello');
+}
+
+const userName = 'Lena';
+
+export { seyHello, userName };
+
 export default sum;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import sum from './helpers/helpers';
+import { seyHello, userName } from './helpers/helpers';
 
 console.log(sum(2, 5));
