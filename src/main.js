9e1ecee9a7bece733076c3a8d45b0a3765293481
Artem Strelnikov <piramida1237@gmail.com>
2025-09-02 11:31:47 +0200

    import_in_main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import sum from './helpers/helpers';
-import { seyHello, userName } from './helpers/helpers';
+import { seyHello, userName as user } from './helpers/helpers';
 
 seyHello();
 
-console.log(userName);
+// console.log(userName);
 
 console.log(sum(2, 5));
+
+console.log(user);
