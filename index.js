924df8a58df328640c32ef5fee988d7e67b4c62b
strelnikovartem <piramida1237@gmail.com>
2025-09-02 09:32:07 +0000

    Deploying to gh-pages from @ strelnikovartem/test@9e1ecee9a7bece733076c3a8d45b0a3765293481 🚀

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */function l(o,e){return o+e}function n(){console.log("hello")}const s="Lena";n();console.log(s);console.log(l(2,5));
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */function l(o,e){return o+e}function n(){console.log("hello")}const s="Lena";n();console.log(l(2,5));console.log(s);
 //# sourceMappingURL=index.js.map
