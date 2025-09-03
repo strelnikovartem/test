05864eefc42657d4073aa88161312b4d89292dbc
Artem Strelnikov <piramida1237@gmail.com>
2025-09-03 09:27:58 +0200

    helpers as

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import sum from './helpers/helpers';
-import { seyHello, userName as user } from './helpers/helpers';
+// import { seyHello, userName as user } from './helpers/helpers';
+import * as help from './helpers/helpers';
 
 seyHello();
 
 // console.log(userName);
 
 console.log(sum(2, 5));
-
-console.log(user);
