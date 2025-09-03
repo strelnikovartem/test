8917b18e0c6b3e41026ee679accda8d83b67b694
Artem Strelnikov <piramida1237@gmail.com>
2025-09-03 09:30:30 +0200

    * ggg

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import sum from './helpers/helpers';
 // import { seyHello, userName as user } from './helpers/helpers';
 import * as help from './helpers/helpers';
 
-seyHello();
+console.log();
+
+help.seyHello();
 
 // console.log(userName);
 
 console.log(sum(2, 5));
+
+console.log(help.userName);
